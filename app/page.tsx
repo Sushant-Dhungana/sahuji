dc5e9355d961b5262dadb4dfd0db1d7a6b0180b7
Sushant-Dhungana <sushantdhungana29@gmail.com>
2025-08-17 15:09:22 +0545

    customization

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
-  return <div>I am home</div>;
+  return (
+    <div className="flex flex-col w-[200px] gap-y-4 mt-20">
+      <Button variant="elevated">I am a button</Button>
+      <Input placeholder="I am an input" />
+      <Progress value={50} className="w-full" />
+      <Textarea placeholder="I am a textarea" />
+      <Checkbox />
+    </div>
+  );
 }
