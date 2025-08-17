3017cab7bfd6b65adc46ce80982c4ff0e20303bd
Sushant-Dhungana <sushantdhungana29@gmail.com>
2025-08-17 21:50:36 +0545

    home page with navbar and footer

---
+++ app/(home)/Navbar.tsx
@@ -0,0 +1,96 @@
+"use client";
+import * as React from "react";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import { NavbarSidebar } from "./navbar-sidebar";
+import { MenuIcon } from "lucide-react";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
+interface NavbarItemProps {
+  href: string;
+  children: React.ReactNode;
+  isActive?: boolean;
+}
+
+const NavbarItem = ({ href, children, isActive }: NavbarItemProps) => {
+  return (
+    <Button
+      variant="outline"
+      className={cn(
+        "bg-white hover:bg-slate-300 rounded-md hover:border-primary border-transparent",
+        isActive &&
+          "bg-slate-300 text-white hover:bg-slate-300 hover:text-white"
+      )}
+    >
+      <Link href={href} className="text-black">
+        {children}
+      </Link>
+    </Button>
+  );
+};
+
+const navbarItems = [
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
+  { href: "/features", label: "Features" },
+  { href: "/pricing", label: "Pricing" },
+  { href: "/contact", label: "Contact" },
+];
+export const Navbar = () => {
+  const pathname = usePathname();
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
+  return (
+    <nav className="bg-gray-800 p-4 items-center flex flex-row justify-between">
+      <Link href="/" className="pl-6 items-center" passHref>
+        <span
+          className={cn("text-2xl font-semibold text-white", poppins.className)}
+        >
+          Sushant
+        </span>
+      </Link>
+      <div className="items-center gap-4 hidden lg:flex">
+        {navbarItems.map((item) => (
+          <NavbarItem
+            key={item.href}
+            href={item.href}
+            isActive={pathname === item.href}
+          >
+            <Link href={item.href} className="text-black">
+              {item.label}
+            </Link>
+            <NavbarSidebar
+              open={isSidebarOpen}
+              onOpenChange={setIsSidebarOpen}
+              items={navbarItems}
+            />
+          </NavbarItem>
+        ))}
+      </div>
+      <div className="hidden lg:flex items-center gap-2">
+        <Button variant="outline" className="text-white bg-red-500" asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+        <Button variant="default" className="text-white" asChild>
+          <Link href="/signup">Sign Up</Link>
+        </Button>
+      </div>
+      <div className="flex lg:hidden  items-center justify-center">
+        <Button
+          variant="ghost"
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="size-12 border-transparent bg-white"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+    </nav>
+  );
+};
